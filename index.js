10eeb1c16b815ecb5fd75985cee966c18c721e4c
Oliver Hsu <mark_rhett@yahoo.com>
2016-12-08 10:58:29 +0800

    database support

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const App = require("./lib/App");
+const DatabaseProxy = require("./lib/db/DatabaseProxy")
 
 exports.App = () => {
   return new App();
+};
+
+exports.DatabaseProxy = () => {
+  return new DatabaseProxy();
 };
